1278ddac7e6dad3e933c87e8e5cfe16e6049ad15
greenine <greenine.dev@gmail.com>
2013-01-27 15:06:32 +0900

    ビュー追加 models追加

---
+++ models.js
@@ -0,0 +1,39 @@
+﻿var mongoose = require('mongoose');
+var topicjsDb = mongoose.connect('mongodb://localhost/topicjs');
+
+var topicSchema = new mongoose.Schema({
+	channel:String,
+	topic:{
+		c:String,
+		m:String
+	},
+	create_at: { type: Date, default: Date.now }
+});
+var streamSchema = new mongoose.Schema({
+	channel:String,
+	stream:String
+});
+var twiConfSchema = new mongoose.Schema({
+	name:String,
+	consumerKey:String,
+	consumerSecret:String,
+	accessTokenKey:String,
+	accessTokenSecret:String
+});
+var streamLogSchema = new mongoose.Schema({
+	channel:String,
+	username:String,
+	userIp:String,
+	createAt: { type: Date, default: Date.now }
+});
+var userSchema = new mongoose.Schema({
+	username:String,
+	password:String,
+	createAt: { type: Date, default: Date.now }
+});
+
+exports.topic = topicjsDb.model('topic', topicSchema);
+exports.stream = topicjsDb.model('stream', streamSchema);
+exports.twiConf = topicjsDb.model('twiConf', twiConfSchema);
+exports.streamLog = topicjsDb.model('streamLog', streamLogSchema);
+exports.user = topicjsDb.model('user', userSchema);
